6b9468c91321041605d001ac490af9178cfc9b89
springbranchxie <xiesj76@gmail.com>
2022-06-13 16:41:32 -0400

    change on 6/13/2022

--- src/test/java/step/definition/DesktopsStepDef.java
+++ src/test/java/step/definition/DesktopsStepDef.java
@@ -40,100 +40,100 @@ public class DesktopsStepDef extends Base  {
 	
 	//Scenario: User add HP LP 3065  from Desktops tab to the cart
 	@When("User click  ADD TO CART option on ‘HP LP3065’ item")
-	public void user_click_add_to_cart_option_on_hp_lp3065_item() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_click_add_to_cart_option_on_hp_lp3065_item() {
+	
 		desktopsPage.AddHP3065();
 	   logger.info("User click  ADD TO CART option on ‘HP LP3065’ item");
 	}
 	@When("User select quantity {int}")
-	public void user_select_quantity(Integer int1) throws InterruptedException {
-		Thread.sleep(5000);
+	public void user_select_quantity(Integer int1)  {
+		
 		desktopsPage.UserSelectQuantity();
 		logger.info("User select quantity {int}");
 		
 	}
 	@When("User click add to Cart button")
-	public void user_click_add_to_cart_button() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_click_add_to_cart_button()  {
+		
 		desktopsPage.AddtoCartButton();
 		logger.info("User click add to Cart button");
 	}
 	@Then("User should see a message ‘Success: you have added HP LP {int} to your Shopping cart!’")
-	public void user_should_see_a_message_success_you_have_added_hp_lp_to_your_shopping_cart(Integer int1) throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_should_see_a_message_success_you_have_added_hp_lp_to_your_shopping_cart(Integer int1)  {
+		
 		desktopsPage.UserShouldSeeMessage();
 		logger.info("User should see a message ‘Success: you have added HP LP {int} to your Shopping cart!’");
 	}
 
 	//Scenario: User add Canon EOS 5D from Desktops tab to the cart
 	@When("User click ADD TO CART option on ‘Canon EOS 5D’ item")
-	public void user_click_add_to_cart_option_on_canon_eos_5d_item() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_click_add_to_cart_option_on_canon_eos_5d_item()  {
+		
 		desktopsPage.AddCanonEOS5DCamera();
 		logger.info("User click ADD TO CART option on ‘Canon EOS 5D’ item");
 	    
 	}
 	@When("User select color from dropdown ‘Red’")
-	public void user_select_color_from_dropdown_red() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_select_color_from_dropdown_red()  {
+		
 		desktopsPage.SelectColorRed();
 		logger.info("User select color from dropdown ‘Red’");
 	}
 	@And("User select Canon quantity 1")
-	public void user_select_Canon_quantity_1() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_select_Canon_quantity_1()  {
+		
 		desktopsPage.UserSelectCanonQuantity();
 		logger.info("User select Canon quantity 1");
 		
 	}
 	@And("User click Add to Cart button")
-	public void User_click_Add_to_Cart_button() throws InterruptedException {
-		Thread.sleep(3000);
+	public void User_click_Add_to_Cart_button()  {
+		
 		desktopsPage.AddCanontoCartButton();
 		logger.info("User click Add to Cart button");
 		
 	}
 	
 	@Then("User should see a message ‘Success: You have added Canon EOS 5D to your shopping cart!’")
-	public void user_should_see_a_message_success_you_have_added_canon_eos_5d_to_your_shopping_cart() throws InterruptedException {
-		Thread.sleep(5000);
+	public void user_should_see_a_message_success_you_have_added_canon_eos_5d_to_your_shopping_cart() {
+		
 		desktopsPage.UserSeeMessage();
 		logger.info("User should see a message ‘Success: You have added Canon EOS 5D to your shopping cart!’");
 	    
 	}
 //Scenario: User add a review to Canon EOS 5D item in Desktops tab
 	@When("User click on Canon EOS 5D item")
-	public void user_click_on_canon_eos_5d_item() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_click_on_canon_eos_5d_item()  {
+		
 		desktopsPage.ClickOnCanonEOS5DItem();
 		logger.info("User click on Canon EOS 5D item");
 	    
 	}
 	@When("User click on write a review link")
-	public void user_click_on_write_a_review_link() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_click_on_write_a_review_link()  {
+		
 		desktopsPage.ClickonWriteReviweLink();
 		logger.info("User click on write a review link");
 		
 	   }
 	@When("user fill the review information with below information")
-	public void user_fill_the_review_information_with_below_information() throws InterruptedException {
-		Thread.sleep(3000);
+	public void user_fill_the_review_information_with_below_information()  {
+		
 		desktopsPage.EnterYourName("james1");
 		desktopsPage.EnterYourReview("this is very good item and I am really like it and have it ,thanks");
 		desktopsPage.RatingOption("notbad");
 		logger.info("user fill the review information with below information");
 	}
 	@When("User click on Continue Button")
-	public void user_click_on_continue_button() throws InterruptedException {
-	    Thread.sleep(3000);
+	public void user_click_on_continue_button()  {
+	
 		desktopsPage.Continue();
 		logger.info("User click on Continue Button");
 	    
 	}
 	@Then("User should see a message with ‘Thank you for your review. It has been submitted to the webmaster for approval.”")
-	public void user_should_see_a_message_with_thank_you_for_your_review_it_has_been_submitted_to_the_webmaster_for_approval() throws InterruptedException {
-	    Thread.sleep(4000);
+	public void user_should_see_a_message_with_thank_you_for_your_review_it_has_been_submitted_to_the_webmaster_for_approval() {
+	   
 		desktopsPage.UserSeeReviewMess();
 		logger.info("User should see a message with ‘Thank you for your review. It has been submitted to the webmaster for approval.”");
 			
